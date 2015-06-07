beb287332a62891ca601fd0a33de7b6fce527ca2
Ross Wilson <rzzzwilson@gmail.com>
2015-06-07 10:29:12 +0700

    Ready for pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='pyslip',
+      version='3.0.0',
+      description='A slipmap widget for wxPython',
+      long_description=readme(),
+      url='http://github.com/rzzzwilson/pySlip',
+      author='Ross Wilson',
+      author_email='rzzzwilson@gmail.com',
+      license='MIT',
+      packages=['pyslip'],
+      install_requires=['python', 'wxpython'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2 :: Only'],
+      keywords='python wxpython slipmap map',
+      zip_safe=False)

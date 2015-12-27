705b35a831b4bdc65e6c5aadb3c54642301de4eb
Ross Wilson <rzzzwilson@gmail.com>
2015-12-27 17:34:57 +0700

    Prepare for 3.1 release

--- pyslip/__init__.py
+++ pyslip/__init__.py
@@ -5,13 +5,6 @@
 import os
 import sys
 
-__version__ = '3.0'
+__version__ = '3.1'
 
 from pyslip import *
-
-
-## add built-in tilesets to sys.path
-#DefaultTilesets = 'tilesets'
-#CurrentPath = os.path.dirname(os.path.abspath(__file__))
-#
-#sys.path.append(os.path.join(CurrentPath, DefaultTilesets))
